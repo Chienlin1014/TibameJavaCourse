80a3873d819dc39f9aa64a55db72121513860f7f
Chienlin1014 <110914314+Chienlin1014@users.noreply.github.com>
2022-08-19 13:28:53 +0800

    add question title

--- Tibame_Java_course/src/homework5/DrawRectangle.java
+++ Tibame_Java_course/src/homework5/DrawRectangle.java
@@ -1,5 +1,7 @@
 package homework5;
 
+import java.util.Scanner;
+//請設計一個方法為starSquare(int width, int height),當使用者鍵盤輸入寬與高時,即會印出對應的*長方形
 public class DrawRectangle {
     //用*號畫出長方形
     public static void starSqiare(int width, int height) {
@@ -11,6 +13,11 @@ public class DrawRectangle {
         }
     }
     public static void main(String[] args) {
-        starSqiare(5,4);
+        System.out.print("請輸入寬：");
+        Scanner sc =new Scanner(System.in);
+        int width = sc.nextInt();
+        System.out.println("請輸入高：");
+        int height = sc.nextInt();
+        starSqiare(width,height);
     }
 }
--- Tibame_Java_course/src/homework5/RanAvg.java
+++ Tibame_Java_course/src/homework5/RanAvg.java
@@ -1,5 +1,5 @@
 package homework5;
-
+//請設計一個方法為randAvg(),從10個 0~100(含100)的整數亂數中取平均值並印出這10個亂數與平均值,如圖:
 public class RanAvg {
     public static void ranAvg() {
         int[] num=new int[10];
