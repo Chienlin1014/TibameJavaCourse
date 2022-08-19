687ec40d5a13c4faffb1b81a3a62a6c5f815e68f
Chienlin1014 <110914314+Chienlin1014@users.noreply.github.com>
2022-08-19 11:36:10 +0800

    javatet

---
+++ Tibame_Java_course/src/homework5/RanAvg.java
@@ -0,0 +1,15 @@
+package homework5;
+
+public class RanAvg {
+    public static void main(String[] args) {
+        int[] num=new int[10];
+        int sum=0;
+        System.out.println("本次亂數結果：");
+        for (int i = 0; i < num.length; i++) {
+            num[i] = (int) (Math.random() * 101);
+            sum+=num[i];
+            System.out.print(num[i]+" ");
+        }
+        System.out.println("平均："+(sum/num.length));
+    }
+}
---
+++ Tibame_Java_course/src/homework5/RandomString.java
@@ -0,0 +1,20 @@
+package homework5;
+
+public class RandomString {
+    public static void getAuthCode() {
+        int num;
+        for (int i = 1; i < 8; i++) {
+            num = (int) (Math.random() * 75 + 48);
+            while ((num > '9' && num < 'A') || (num > 'Z' && num < 'a')) {
+                num = (int) (Math.random() * 75 + 48);
+            }
+//            while ((num>57&&num<65)||(num>90&&num<97)){
+//                num=(int)(Math.random()*75+48);
+//            }
+            System.out.print((char) num);
+        }
+    }
+    public static void main(String[] args) {
+        getAuthCode();
+    }
+}
