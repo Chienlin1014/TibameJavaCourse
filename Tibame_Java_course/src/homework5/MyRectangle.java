846ee69aaa9fde2247e76c13f2992719c7f0daf9
Chienlin1014 <110914314+Chienlin1014@users.noreply.github.com>
2022-08-19 13:08:51 +0800

    javatet

--- Tibame_Java_course/src/homework5/MyRectangle.java
+++ Tibame_Java_course/src/homework5/MyRectangle.java
@@ -27,7 +27,7 @@ public class MyRectangle {
         mr[0].setDepth(20);
         System.out.println("無參數建構子");
         mr[0].getArea(); //無輸入參數
-        System.out.println("參數建構子");
+        System.out.println("有參數建構子");
         mr[1] = new MyRectangle(10, 20);
         mr[1].getArea();
     }
