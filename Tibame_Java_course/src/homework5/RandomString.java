67c50569dd518eba581c1cdf7772ea57bee091d9
Chienlin1014 <110914314+Chienlin1014@users.noreply.github.com>
2022-08-19 13:16:24 +0800

    javatet

--- Tibame_Java_course/src/homework5/RandomString.java
+++ Tibame_Java_course/src/homework5/RandomString.java
@@ -8,6 +8,7 @@ public class RandomString {
             while ((num > '9' && num < 'A') || (num > 'Z' && num < 'a')) {
                 num = (int) (Math.random() * 75 + 48);
             }
+//            可用下列式子取代，數字代表的是unicode編碼
 //            while ((num>57&&num<65)||(num>90&&num<97)){
 //                num=(int)(Math.random()*75+48);
 //            }
