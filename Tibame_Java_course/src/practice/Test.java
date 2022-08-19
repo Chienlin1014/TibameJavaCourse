dd8acaaea389fa23ef2aabfdd8adbff71cbf282f
Chienlin1014 <110914314+Chienlin1014@users.noreply.github.com>
2022-08-19 17:27:05 +0800

    modified

--- Tibame_Java_course/src/practice/Test.java
+++ Tibame_Java_course/src/practice/Test.java
@@ -6,43 +6,12 @@ import java.util.Scanner;
 public class Test {
 
 	public static void main(String[] args) {
-// 		阿文很喜歡簽大樂透(1～49)，但他是個善變的人，上次討厭數字是4，但這次他想要依心情決定討
-// 		厭哪個數字，請您設計一隻程式，讓阿文可以輸入他不想要的數字(1～9)，畫面會顯示他可以選擇
-// 		的號碼與總數，如圖：
-// 		(提示：Scanner)
-// 		(進階挑戰：輸入不要的數字後，直接亂數印出6個號碼且不得重複)
-
- 		int num, count = 0;
- 		Scanner cin = new Scanner(System.in);
- 		System.out.println("輸入不想要的數字 :");
- 		int c = cin.nextInt();
- 		
- 		for (num = 1; num <= 49; num++) {
- 			if (num % 10 == c || num / 10 == c) {
- 			
- 			}
- 			if (count % 7 == 0) {
- 				System.out.println();
- 			}
- 			System.out.print(num + "\t");
- 			count++;
- 		}
- 		System.out.println();
- 		System.out.print("總共可以選 :" + count + "個號碼");
- 		System.out.println("隨機生成 六 號碼:");
- 
-         Random r = new Random();
- 		int[] ran = new int[6];
- 		for (int i = 0; i < ran.length; i++) {
- 			ran[i] = (int) (Math.random() * 49) + 1;
- 			for (int j = 0; j < i; j++) {
- 				if (ran[i] == ran[j] || ran[i] == c) {
- 					ran[i] --;
- 					continue;
- 				}
- 			}
- 		   System.out.print(ran[i] + " ");
- 		}
+		Integer i1 = new Integer(10);
+		Integer i2 = new Integer(10);
+		System.out.println(i1==i2);
+		System.out.println(i1.equals(i2));
+		Integer i = Integer.valueOf(10);
+		
 	}
 
 }
