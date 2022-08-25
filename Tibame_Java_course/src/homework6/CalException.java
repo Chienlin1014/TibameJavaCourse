204e263203ead010f1bd57a76b294721a456d64f
Chienlin1014 <shzerokimo@gmail.com>
2022-08-25 22:41:26 +0800

    modified

--- Tibame_Java_course/src/homework6/CalException.java
+++ Tibame_Java_course/src/homework6/CalException.java
@@ -1,6 +1,6 @@
 package homework6;
 
-public class CalException extends RuntimeException{
+public class CalException extends Exception{
     public CalException(String str) {
         super(str);
     }
